c19475732e561efb43c76a9920df46ca8adc2b52
Javad <jalizade3d@yahoo.com>
2018-12-04 12:12:28 +0100

    ejb5

--- README.md
+++ README.md
@@ -2,62 +2,66 @@
 
 Source: [The Java EE Course - Build a Java EE App From Scratch](https://www.udemy.com/the-java-ee-course/)
 
-##### Web #1:
+#### Web #1:
 - very fisrt configs
 - direct html data sent via response
 
-##### Web #2:
+#### Web #2:
 - `views` added
 - forwarding to `add_passenger.jsp`
 
-##### Web #3:
+#### Web #3:
 - form added
 - POST & GET definition
 
-##### Web #4:
+#### Web #4:
 - normalize.css & theme.css added
 
-##### Web #5:
+#### Web #5:
 - Passenger & Gender classes added
 
-##### Web #6:
+#### Web #6:
 - getting the data sent from the form
 
-##### Web #7:
+#### Web #7:
 - form validation
 - `request.setAttribute()`
 
-##### Web #8:
+#### Web #8:
 - showing form error messages
 - `theme.css` > `li.error`
 
-##### Web #9:
+#### Web #9:
 - handling successfull form submission #1
 - `response.sendRedirect("");`
 - `MainPage`
 
-##### Web #10:
+#### Web #10:
 - creating a Servlet Context Listener and adding the passenger list to the context
 - `AirlineListener`
 
-##### Web #11:
+#### Web #11:
 - Synchronization and Thread Safety when adding new passengers
 - BUG fixed: the `pList` creation was miswritten in `AirlineListener`
 
-##### Web #12:
+#### Web #12:
 - Improving form validation
 - Bug fixed: misspell in class `Passenger`, `dao` changed to `dob`
 
-##### EJB #1:
+#### EJB #1:
 - Creating a Stateless Session Bean
 - `com.airline.service.FlightService`
 
-##### EJB #2:
+#### EJB #2:
 - Injecting an EJB into a Servlet
 - `com.jalizadeh.controllers.FlightDetails` >	BUG: line #27 must be uncommented
 
-##### EJB #3:
+#### EJB #3:
 - Nature of Stateless Session Enterprise Java Beans - part I
 
-##### EJB #4:
+#### EJB #4:
 - Nature of Stateless Session Enterprise Java Beans - part II
+
+#### EJB #5:
+- Looking up EJBs through Java Naming and Directory Interface JNDI
+- `@EJB` is commented
---
+++ ejb5/src/com/jalizadeh/controllers/FlightDetails.java
@@ -0,0 +1,63 @@
+package com.jalizadeh.controllers;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.ejb.EJB;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.airline.service.FlightService;
+
+/**
+ * Servlet implementation class FlightDetails
+ */
+@WebServlet("/FlightDetails")
+public class FlightDetails extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	//@EJB
+	private FlightService fs = null;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public FlightDetails() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		
+		try {
+			Context context = new InitialContext();
+			Object obj = context.lookup("java:global/ejb5/FlightService!com.airline.service.FlightService");
+			fs = (FlightService) obj;
+			
+		} catch (NamingException e) {
+			System.err.println("There was an error finding FlightService EJB object");
+			e.getStackTrace();
+		}
+		
+		out.println("Flight details: "+ fs.getFrom() + " -> " + fs.getTo());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
